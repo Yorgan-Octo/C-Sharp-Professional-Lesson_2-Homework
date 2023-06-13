e48e8f737d49c350eaad8a1247c8773138f4d675
Artem Priadchenko <yorgan.info@gmail.com>
2023-06-14 00:51:33 +0300

    Добавьте файлы проекта.

---
+++ Task_4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Program
+    {
+
+        public class Comparison : IEqualityComparer
+        {
+
+            CaseInsensitiveComparer comparer = new CaseInsensitiveComparer();
+
+            public int GetHashCode(object obj)
+            {
+                return obj.ToString().ToLowerInvariant().GetHashCode();
+            }
+
+            public new bool Equals(object x, object y)
+            {
+                return comparer.Compare(x, y) == 0;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            OrderedDictionary elemenBase = new OrderedDictionary(new Comparison())
+            {
+                { "Магнус Карлсен", 2847 },
+                { "Фабиано Каруана", 2820 },
+                { "Левон Аронян", 2797 },
+                { "Вишванатан Ананд", 2753 },
+                { "Шхрияр Мамедьяров", 2750 }
+             };
+
+            foreach (DictionaryEntry entry in elemenBase)
+            {
+                Console.WriteLine($"{entry.Key} - {entry.Value}");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public static class Extenthis
+    {
+        public static bool KeyEquals<T, R>(this OrderedDictionary left, OrderedDictionary right)
+        {
+           return left.Values == right.Values;
+        }
+    }
+
+}
---
+++ Task_6/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+
+    public class SortComparer<T> : IComparer<T>
+    {
+
+        CaseInsensitiveComparer comparer = new CaseInsensitiveComparer();
+
+        public int Compare(T x, T y)
+        {
+            return comparer.Compare(y, x);
+        }
+
+
+    }
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+
+            SortedList<string, int> myColection = new SortedList<string, int>();
+
+            myColection.Add("Магнус Карлсен", 2847);
+            myColection.Add("Фабиано Каруана", 2820);
+            myColection.Add("Левон Аронян", 2797);
+            myColection.Add("Вишванатан Ананд", 2753);
+            myColection.Add("Шхрияр Мамедьяров", 2750);
+
+
+            SortedList<string, int> myColectionRevers = new SortedList<string, int>(new SortComparer<string>());
+
+            myColectionRevers.Add("Магнус Карлсен", 2847);
+            myColectionRevers.Add("Фабиано Каруана", 2820);
+            myColectionRevers.Add("Левон Аронян", 2797);
+            myColectionRevers.Add("Вишванатан Ананд", 2753);
+            myColectionRevers.Add("Шхрияр Мамедьяров", 2750);
+
+
+            foreach (var item in myColection)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+            Console.WriteLine(new String('-', 50));
+
+            foreach (var item in myColectionRevers)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+
+            Console.WriteLine(new String('-', 50));
+
+            //як вариант 
+            myColection.IsDescendingSort();
+
+
+            Console.WriteLine(new String('-', 50));
+
+            //туплватый але як вариант 
+            myColection.IsDesForSort();
+
+            Console.ReadKey();
+        }
+
+    }
+
+    public static class Extenthis
+    {
+        public static void IsDescendingSort<T, R>(this SortedList<T, R> sortedList)
+        {
+            var temp = sortedList;
+
+            sortedList = new SortedList<T, R>(new SortComparer<T>());
+
+            foreach (var item in temp)
+            {
+                sortedList.Add(item.Key, item.Value);
+            }
+            foreach (var item in sortedList)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+
+
+        public static void IsDesForSort<T, R>(this SortedList<T, R> sortedList)
+        {
+            T[] key = new T[sortedList.Count];
+            R[] val = new R[sortedList.Count];
+
+            int i = sortedList.Count -1;
+            foreach (var item in sortedList)
+            {
+                key[i] = item.Key;
+                val[i] = item.Value;
+                i--;
+            }
+
+            key.Reverse();
+            val.Reverse();
+
+            for (int j = 0; j < sortedList.Count; j++)
+            {
+                Console.WriteLine($"{key[j]} - {val[j]}");
+            }
+        }
+    }
+}
